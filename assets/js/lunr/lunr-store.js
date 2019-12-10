cf4cec9807e4698507769a79d04573c478809c8f
NoahPeeters <NoahPeeters@users.noreply.github.com>
2019-12-10 13:21:05 +0000

    Automated deployment: Tue Dec 10 13:21:05 UTC 2019 d2a7e273e10e31d79a8cc756d3ddf873f5e7e70c

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,145 @@
+var store = [
+
+      {
+        "title": "Blog posts",
+        "excerpt": "My blog",
+        "fulltext": "In this blog, I might irregularly post stuff.   2019                                Hello World                                  less than 1 minute read             I just created this website. The main goal is to present me and my projects on the internet.",
+        "categories": null,
+        "tags": null,
+        "url": "https://www.noahpeeters.de/blog/",
+        "teaser":null},
+
+      {
+        "title": "CV",
+        "excerpt": "My CV",
+        "fulltext": "Name          E-Mail          Phone          Homepage          Location          Citizenship                   Noah N. Peeters          noah@noahpeeters.de          +49 171 8952156‬          noahpeeters.de          Geesthacht, Germany          German                                                                                                                                         [Printable Version]   Education     Primary School, Silberbergschule, August 2005 to July 2009   Abitur, Otto-Hahn-Gymnasium Geesthacht, August 2009 to June 2017   B.S. in Applied Computer Science, Nordakademie Hochschule der Wirtschaft, October 2017 to March 2021 (expected)   Scholarships     WWDC19 Scholarship, Apple Inc., June 2019   Intercultural Communication Summer Institute, Fulbright, July 2019   Work experience     Internship, Computacenter AG &amp; Co oHG, March 2014   Internship, expert AG, November 2015   iOS Developer, freenet.de GmbH, October 2017 to present   Technical Skills     iOS development   Publications           Zusammenfassung Oberstufe Physik              Open Source Software - Wie nehmen Endanwender Open Source Software wahr?              Wie können UI-Tests die Qualitätssicherung von iOS Applikation verbessern?              Wie kann CodePush für die Verbreitung von React Native Apps eingesetzt werden?              Google Analytics for mobile apps shutdown: Analysis of the implication of switching to Google Firebase.       Teaching           Various Tutorings              Tutorial Discrete Mathematics 1       Languages     :de: German (native)   :us: English (oral and written fluency)   :fr: French (DELF A2)   Service and leadership     JAV (Youth and apprentice representation) for freenet AG @ Hamburg since November 2019   Board member of the student organisation “Studentengemeinschaft Nordakademie e.V.” since February 2018   Memberships     Fulbright Alumni e.V., The German Fulbright Alumni Association",
+        "categories": null,
+        "tags": null,
+        "url": "https://www.noahpeeters.de/cv/",
+        "teaser":null},
+
+      {
+        "title": "Noah Peeters",
+        "excerpt": "Welcome to the front page of my website.",
+        "fulltext": "Welcome to the front page of my website.",
+        "categories": null,
+        "tags": null,
+        "url": "https://www.noahpeeters.de/",
+        "teaser":null},
+
+      {
+        "title": "Portfolio",
+        "excerpt": "My public projects",
+        "fulltext": "In my free time, I always have some side-projects. Here you can find a list of projects I published in recent years.",
+        "categories": null,
+        "tags": null,
+        "url": "https://www.noahpeeters.de/portfolio/",
+        "teaser":null},
+
+      {
+        "title": "Publications",
+        "excerpt": "My publication",
+        "fulltext": "",
+        "categories": null,
+        "tags": null,
+        "url": "https://www.noahpeeters.de/publications/",
+        "teaser":"https://www.noahpeeters.de/assets/images/my-awesome-post-teaser.jpg"},
+
+      {
+        "title": "Teaching",
+        "excerpt": "My teaching experiences",
+        "fulltext": "",
+        "categories": null,
+        "tags": null,
+        "url": "https://www.noahpeeters.de/teaching/",
+        "teaser":null},
+
+      {
+        "title": "FidSpeed",
+        "excerpt": "iOS app to measure the speed of fidget spinners",
+        "fulltext": "FidSpeed is an iOS app to measure the speed of Fidget Spinners which became very popular in 2017.      How does it work?   FidSpeed uses the iPhones slow-motion camera to capture the motion of Fidget Spinners in real-time. From each frame, the current angle of the Fidget Spinner is determined, which can be used to calculate the speed.",
+        "categories": [],
+        "tags": [],
+        "url": "https://www.noahpeeters.de/portfolio/fidspeed/",
+        "teaser":null},
+
+      {
+        "title": "Wobbly Tower",
+        "excerpt": "Build a tower using tetrominos (the shapes from tetris)",
+        "fulltext": "Wobbly Tower was my first iOS game project on the AppStore. The goal of the game is to stack the different tetrominos and build a tower.",
+        "categories": [],
+        "tags": [],
+        "url": "https://www.noahpeeters.de/portfolio/wobbly_tower/",
+        "teaser":null},
+
+      {
+        "title": "Hello World",
+        "excerpt": "<p>I just created this website. The main goal is to present me and my projects on the internet.</p>\n",
+        "fulltext": "I just created this website. The main goal is to present me and my projects on the internet.",
+        "categories": [],
+        "tags": [],
+        "url": "https://www.noahpeeters.de/blog/2019-12-hello-world/",
+        "teaser":null},
+
+      {
+        "title": "Zusammenfassung Oberstufe Physik",
+        "excerpt": "In this paper, I summarized all topics we discussed in school in physics.",
+        "fulltext": "In this paper, I summarized all topics we discussed in school in physics.   Goal   The goal of the paper is to give students an overview of all topics. This allows them to practice for the Abitur using this paper.   The paper does not have the goal to teach new students the topic and I discourage anyone from trying as the explanations are not broad enough to   Paper structure   The paper uses the same structure as the class in school. The paper contains explanations for each topic.",
+        "categories": [],
+        "tags": [],
+        "url": "https://www.noahpeeters.de/publications/oberstufe-physik/",
+        "teaser":null},
+
+      {
+        "title": "Open Source Software - Wie nehmen Endanwender Open Source Software wahr?",
+        "excerpt": "Survey on the perception of Open Source Software.",
+        "fulltext": "Survey on the perception of Open Source Software.",
+        "categories": [],
+        "tags": [],
+        "url": "https://www.noahpeeters.de/publications/open-source-software/",
+        "teaser":null},
+
+      {
+        "title": "Wie können UI-Tests die Qualitätssicherung von iOS Applikation verbessern?",
+        "excerpt": "This is my first Transferleistung.",
+        "fulltext": "This is my first Transferleistung.",
+        "categories": [],
+        "tags": [],
+        "url": "https://www.noahpeeters.de/publications/transferleistung1/",
+        "teaser":null},
+
+      {
+        "title": "Wie kann CodePush für die Verbreitung von React Native Apps eingesetzt werden?",
+        "excerpt": "This is my second Transferleistung.",
+        "fulltext": "This is my second Transferleistung.",
+        "categories": [],
+        "tags": [],
+        "url": "https://www.noahpeeters.de/publications/transferleistung2/",
+        "teaser":null},
+
+      {
+        "title": "Google Analytics for mobile apps shutdown: Analysis of the implication of switching to Google Firebase.",
+        "excerpt": "This is my third Transferleistung.",
+        "fulltext": "This is my third Transferleistung.",
+        "categories": [],
+        "tags": [],
+        "url": "https://www.noahpeeters.de/publications/transferleistung3/",
+        "teaser":null},
+
+      {
+        "title": "Various Tutorings",
+        "excerpt": "<p>Since 2015 during my time at grammar school, I regularly gave other students tutoring in mathematics and physics.</p>\n",
+        "fulltext": "Since 2015 during my time at grammar school, I regularly gave other students tutoring in mathematics and physics.",
+        "categories": [],
+        "tags": [],
+        "url": "https://www.noahpeeters.de/teaching/tutoring",
+        "teaser":null},
+
+      {
+        "title": "Tutorial Discrete Mathematics 1",
+        "excerpt": "<p>During my third semester of my bachelor’s degree, I was the instructor of the tutorial for “Discrete Mathematics 1”.</p>\n",
+        "fulltext": "During my third semester of my bachelor’s degree, I was the instructor of the tutorial for “Discrete Mathematics 1”.",
+        "categories": [],
+        "tags": [],
+        "url": "https://www.noahpeeters.de/teaching/2019-tutorial-dm1",
+        "teaser":null},]
