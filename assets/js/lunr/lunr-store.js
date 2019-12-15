1807c3c93b8fb70d50728febad875a104905ce80
NoahPeeters <NoahPeeters@users.noreply.github.com>
2019-12-15 19:03:12 +0000

    Automated deployment: Sun Dec 15 19:03:12 UTC 2019 18765749ad1118317b26d2549ee4860d2884932f

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -12,7 +12,7 @@ var store = [
       {
         "title": "CV",
         "excerpt": "My CV",
-        "fulltext": "Name          E-Mail          Phone          Homepage          Location          Citizenship                   Noah N. Peeters          noah@noahpeeters.de          +49 171 8952156‬          noahpeeters.de          Geesthacht, Germany          German                                                                                                                                         [Printable Version]   Education     Primary School, Silberbergschule, August 2005 to July 2009   Abitur, Otto-Hahn-Gymnasium Geesthacht, August 2009 to June 2017   B.S. in Applied Computer Science, Nordakademie Hochschule der Wirtschaft, October 2017 to March 2021 (expected)   Scholarships     WWDC19 Scholarship, Apple Inc., June 2019   Intercultural Communication Summer Institute, Fulbright, July 2019   Work experience     Internship, Computacenter AG &amp; Co oHG, March 2014   Internship, expert AG, November 2015   iOS Developer, freenet.de GmbH, October 2017 to present   Technical Skills     iOS development   Publications           Zusammenfassung Oberstufe Physik              Open Source Software - Wie nehmen Endanwender Open Source Software wahr?              Wie können UI-Tests die Qualitätssicherung von iOS Applikation verbessern?              Wie kann CodePush für die Verbreitung von React Native Apps eingesetzt werden?              Google Analytics for mobile apps shutdown: Analysis of the implication of switching to Google Firebase.       Teaching           Various Tutorings              Tutorial Discrete Mathematics 1       Languages     :de: German (native)   :us: English (oral and written fluency)   :fr: French (DELF A2)   Service and leadership     JAV (Youth and apprentice representation) for freenet AG @ Hamburg since November 2019   Board member of the student organisation “Studentengemeinschaft Nordakademie e.V.” since February 2018   Memberships     Fulbright Alumni e.V., The German Fulbright Alumni Association",
+        "fulltext": "Name          E-Mail          Phone          Homepage          Location          Citizenship                   Noah N. Peeters          noah@noahpeeters.de          +49 171 8952156‬          noahpeeters.de          Geesthacht, Germany          German                                                                                                                                         [Printable Version]   Education     Primary School, Silberbergschule, August 2005 to July 2009   Abitur, Otto-Hahn-Gymnasium Geesthacht, August 2009 to June 2017 #OHG   B.S. in Applied Computer Science, Nordakademie Hochschule der Wirtschaft, October 2017 to March 2021 (expected) #Nordakademie   Scholarships     WWDC19 Scholarship, Apple Inc., June 2019 #WWDC2019   Intercultural Communication Summer Institute, Fulbright, July 2019   Work experience     Internship, Computacenter AG &amp; Co oHG, March 2014   Internship, expert AG, November 2015   iOS Developer, freenet.de GmbH, October 2017 to present   Technical Skills     iOS development   Publications           Zusammenfassung Oberstufe Physik              Open Source Software - Wie nehmen Endanwender Open Source Software wahr?              Wie können UI-Tests die Qualitätssicherung von iOS Applikation verbessern?              Wie kann CodePush für die Verbreitung von React Native Apps eingesetzt werden?              Google Analytics for mobile apps shutdown: Analysis of the implication of switching to Google Firebase.       Teaching           Various Tutorings              Tutorial Discrete Mathematics 1       Languages     :de: German (native)   :us: English (oral and written fluency)   :fr: French (DELF A2)   Service and leadership     JAV (Youth and apprentice representation) for freenet AG @ Hamburg since November 2019   Board member of the student organisation “Studentengemeinschaft Nordakademie e.V.” since February 2018 #Nordakademie   Memberships     Fulbright Alumni e.V., The German Fulbright Alumni Association #Fulbright",
         "categories": null,
         "tags": null,
         "url": "https://www.noahpeeters.de/cv/",
@@ -25,6 +25,15 @@ var store = [
         "categories": null,
         "tags": null,
         "url": "https://www.noahpeeters.de/",
+        "teaser":null},
+
+      {
+        "title": "News",
+        "excerpt": "External news article",
+        "fulltext": "Here I have linked all kinds of articles about me and my projects.",
+        "categories": null,
+        "tags": null,
+        "url": "https://www.noahpeeters.de/news/",
         "teaser":null},
 
       {
@@ -46,6 +55,15 @@ var store = [
         "teaser":"https://www.noahpeeters.de/assets/images/my-awesome-post-teaser.jpg"},
 
       {
+        "title": "Tags",
+        "excerpt": "Content sorted by tags",
+        "fulltext": "",
+        "categories": null,
+        "tags": null,
+        "url": "https://www.noahpeeters.de/tags/",
+        "teaser":null},
+
+      {
         "title": "Teaching",
         "excerpt": "My teaching experiences",
         "fulltext": "",
@@ -55,12 +73,129 @@ var store = [
         "teaser":null},
 
       {
+        "title": "Schüler des Otto-Hahn-Gymnasiums kämpft mit virtuellem Spieler um den Einzug ins Finale",
+        "excerpt": "Article about my participation in the \"Software Challenge\".",
+        "fulltext": "",
+        "categories": [],
+        "tags": ["SoftwareChallange","OHG"],
+        "url": "https://www.noahpeeters.de/news/ohg/software-challenge/loz-news/",
+        "teaser":null},
+
+      {
+        "title": "OHG kämpft um den Finaleinzug",
+        "excerpt": "Article about my participation in the \"Software Challenge\".",
+        "fulltext": "",
+        "categories": [],
+        "tags": ["SoftwareChallange","OHG"],
+        "url": "https://www.noahpeeters.de/news/ohg/software-challenge/bergedorfer-zeitung/",
+        "teaser":null},
+
+      {
+        "title": "New perspective",
+        "excerpt": "Article about filming a sailing boat with a drone.",
+        "fulltext": "",
+        "categories": [],
+        "tags": [],
+        "url": "https://www.noahpeeters.de/news/other/drone-sailing/",
+        "teaser":null},
+
+      {
+        "title": "OHG - Absolventen 2017",
+        "excerpt": "Article about the graduates at my highschool.",
+        "fulltext": "",
+        "categories": [],
+        "tags": ["OHG"],
+        "url": "https://www.noahpeeters.de/news/ohg/abitur/",
+        "teaser":null},
+
+      {
+        "title": "Lange Nacht der Mathematik 2017",
+        "excerpt": "Article about the participation of my team at the \"Lange Nacht der Mathematik\".",
+        "fulltext": "",
+        "categories": [],
+        "tags": ["OHG"],
+        "url": "https://www.noahpeeters.de/news/ohg/lange-nacht-der-mathematik-2017/",
+        "teaser":null},
+
+      {
+        "title": "Von Geesthachts Stadtbücherei zu Apple ins Silicon Valley",
+        "excerpt": "Article about my scholarship for the WWDC 2019.",
+        "fulltext": "",
+        "categories": [],
+        "tags": ["WWDC2019"],
+        "url": "https://www.noahpeeters.de/news/wwdc/bergedorfer-zeitung/",
+        "teaser":null},
+
+      {
+        "title": "WWDC 2019: Apple bewirbt Apps von Stipendiaten",
+        "excerpt": "Article about apps of WWDC 2019 scholars.",
+        "fulltext": "",
+        "categories": [],
+        "tags": ["WWDC2019","AppStore"],
+        "url": "https://www.noahpeeters.de/news/wwdc/macerkopf-apps/",
+        "teaser":null},
+
+      {
+        "title": "Wow: Diese Apps haben die Stipendiaten der WWDC 2019 entwickelt",
+        "excerpt": "Article about apps of WWDC 2019 scholars.",
+        "fulltext": "",
+        "categories": [],
+        "tags": ["WWDC2019","AppStore"],
+        "url": "https://www.noahpeeters.de/news/wwdc/maclife-apps/",
+        "teaser":null},
+
+      {
+        "title": "Vor der WWDC: Apple zeichnet Stipendien-Sieger für innovative Apps aus",
+        "excerpt": "Article about the WWDC 2019 scholars from Germany and Austria.",
+        "fulltext": "",
+        "categories": [],
+        "tags": ["WWDC2019"],
+        "url": "https://www.noahpeeters.de/news/wwdc/mactechnews-participants/",
+        "teaser":null},
+
+      {
+        "title": "Student der Elmshorner NORDAKADEMIE trifft die besten Apple-Entwickler bei Kongress in San Jose",
+        "excerpt": "Article about my scholarship for the WWDC 2019.",
+        "fulltext": "",
+        "categories": [],
+        "tags": ["WWDC2019","Nordakademie"],
+        "url": "https://www.noahpeeters.de/news/wwdc/nordakademie/",
+        "teaser":null},
+
+      {
+        "title": "Nordakademie-Student trifft die besten Apple-Entwickler",
+        "excerpt": "Article about my scholarship for the WWDC 2019.",
+        "fulltext": "",
+        "categories": [],
+        "tags": ["WWDC2019"],
+        "url": "https://www.noahpeeters.de/news/wwdc/shz/",
+        "teaser":null},
+
+      {
+        "title": "FRANKly No. 30 - New Members",
+        "excerpt": "List of new Fulbright Alumni e.V. members.",
+        "fulltext": "",
+        "categories": [],
+        "tags": ["Fulbright"],
+        "url": "https://www.noahpeeters.de/news/fulbright/new-member/",
+        "teaser":null},
+
+      {
+        "title": "Einmal Silicon Valley und zurück",
+        "excerpt": "Interview about my scholarship for the WWDC 2019.",
+        "fulltext": "",
+        "categories": [],
+        "tags": ["WWDC2019","Nordakademie"],
+        "url": "https://www.noahpeeters.de/news/wwdc/nordakademie-campusforum/",
+        "teaser":null},
+
+      {
         "title": "FidSpeed",
         "excerpt": "iOS app to measure the speed of fidget spinners",
         "fulltext": "FidSpeed is an iOS app to measure the speed of Fidget Spinners which became very popular in 2017.      How does it work?   FidSpeed uses the iPhones slow-motion camera to capture the motion of Fidget Spinners in real-time. From each frame, the current angle of the Fidget Spinner is determined, which can be used to calculate the speed.",
         "categories": [],
-        "tags": [],
-        "url": "https://www.noahpeeters.de/portfolio/fidspeed/",
+        "tags": ["AppStore"],
+        "url": "https://www.noahpeeters.de/portfolio/appstore/fidspeed/",
         "teaser":null},
 
       {
@@ -68,8 +203,8 @@ var store = [
         "excerpt": "Build a tower using tetrominos (the shapes from tetris)",
         "fulltext": "Wobbly Tower was my first iOS game project on the AppStore. The goal of the game is to stack the different tetrominos and build a tower.",
         "categories": [],
-        "tags": [],
-        "url": "https://www.noahpeeters.de/portfolio/wobbly_tower/",
+        "tags": ["AppStore"],
+        "url": "https://www.noahpeeters.de/portfolio/appstore/wobbly_tower/",
         "teaser":null},
 
       {
@@ -78,7 +213,7 @@ var store = [
         "fulltext": "I just created this website. The main goal is to present me and my projects on the internet.",
         "categories": [],
         "tags": [],
-        "url": "https://www.noahpeeters.de/blog/2019-12-hello-world/",
+        "url": "https://www.noahpeeters.de/posts/2019-12-07-hello-world/",
         "teaser":null},
 
       {
@@ -86,8 +221,8 @@ var store = [
         "excerpt": "In this paper, I summarized all topics we discussed in school in physics.",
         "fulltext": "In this paper, I summarized all topics we discussed in school in physics.   Goal   The goal of the paper is to give students an overview of all topics. This allows them to practice for the Abitur using this paper.   The paper does not have the goal to teach new students the topic and I discourage anyone from trying as the explanations are not broad enough to   Paper structure   The paper uses the same structure as the class in school. The paper contains explanations for each topic.",
         "categories": [],
-        "tags": [],
-        "url": "https://www.noahpeeters.de/publications/oberstufe-physik/",
+        "tags": ["OHG"],
+        "url": "https://www.noahpeeters.de/publications/ohg/oberstufe-physik/",
         "teaser":null},
 
       {
@@ -95,8 +230,8 @@ var store = [
         "excerpt": "Survey on the perception of Open Source Software.",
         "fulltext": "Survey on the perception of Open Source Software.",
         "categories": [],
-        "tags": [],
-        "url": "https://www.noahpeeters.de/publications/open-source-software/",
+        "tags": ["Nordakademie"],
+        "url": "https://www.noahpeeters.de/publications/nordakademie/open-source-software/",
         "teaser":null},
 
       {
@@ -104,8 +239,8 @@ var store = [
         "excerpt": "This is my first Transferleistung.",
         "fulltext": "This is my first Transferleistung.",
         "categories": [],
-        "tags": [],
-        "url": "https://www.noahpeeters.de/publications/transferleistung1/",
+        "tags": ["Transferleistung","Nordakademie"],
+        "url": "https://www.noahpeeters.de/publications/nordakademie/transferleistung1/",
         "teaser":null},
 
       {
@@ -113,8 +248,8 @@ var store = [
         "excerpt": "This is my second Transferleistung.",
         "fulltext": "This is my second Transferleistung.",
         "categories": [],
-        "tags": [],
-        "url": "https://www.noahpeeters.de/publications/transferleistung2/",
+        "tags": ["Transferleistung","Nordakademie"],
+        "url": "https://www.noahpeeters.de/publications/nordakademie/transferleistung2/",
         "teaser":null},
 
       {
@@ -122,8 +257,8 @@ var store = [
         "excerpt": "This is my third Transferleistung.",
         "fulltext": "This is my third Transferleistung.",
         "categories": [],
-        "tags": [],
-        "url": "https://www.noahpeeters.de/publications/transferleistung3/",
+        "tags": ["Transferleistung","Nordakademie"],
+        "url": "https://www.noahpeeters.de/publications/nordakademie/transferleistung3/",
         "teaser":null},
 
       {
@@ -131,8 +266,8 @@ var store = [
         "excerpt": "<p>Since 2015 during my time at grammar school, I regularly gave other students tutoring in mathematics and physics.</p>\n",
         "fulltext": "Since 2015 during my time at grammar school, I regularly gave other students tutoring in mathematics and physics.",
         "categories": [],
-        "tags": [],
-        "url": "https://www.noahpeeters.de/teaching/tutoring",
+        "tags": ["OHG"],
+        "url": "https://www.noahpeeters.de/teaching/ohg/tutoring/",
         "teaser":null},
 
       {
@@ -140,6 +275,6 @@ var store = [
         "excerpt": "<p>During my third semester of my bachelor’s degree, I was the instructor of the tutorial for “Discrete Mathematics 1”.</p>\n",
         "fulltext": "During my third semester of my bachelor’s degree, I was the instructor of the tutorial for “Discrete Mathematics 1”.",
         "categories": [],
-        "tags": [],
-        "url": "https://www.noahpeeters.de/teaching/2019-tutorial-dm1",
+        "tags": ["Nordakademie"],
+        "url": "https://www.noahpeeters.de/teaching/nordakademie/tutorial-dm1/",
         "teaser":null},]
