b72b0a3176db0136e327f3a90a7f3aa8d6de4500
NoahPeeters <NoahPeeters@users.noreply.github.com>
2020-06-17 21:21:20 +0000

    deploy: cfe1f7a16f66f49ce727929c1519c42f65e2c010

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -421,7 +421,7 @@
       {
         "title": "Noah Peeters",
         "excerpt": "<p><img src=\"https://img.shields.io/uptimerobot/ratio/m784826742-736882641340bace30e4e44a?label=NoahPeeters.de%20Uptime\" alt=\"Uptime Robot ratio (30 days)\" /></p>\n",
-        "fulltext": "Hello there :wave: My name is Noah Peeters and I created this website because I had the domain and I thought it might be funny to have one. You can find the source code on my GitHub account in case you’re interested in it.   I’ll try to maintain a single point of contact for my digital life here. If you want to contact me (anonymously), feel free to use my contact form.",
+        "fulltext": "Hello there :wave: My name is Noah Peeters and I created this website because I had the domain and I thought it might be funny to have one. You can find the source code on my GitHub account in case you’re interested in it.   I’ll try to maintain a single point of contact for my digital life here. If you want to contact me (anonymously), feel free to use my contact form or one of the many platforms I use. You can find them on the left or in the Follow menu on mobile devices.   GitHub contribution chart  What else would you put on the main page of your website?        Github contributions chart. GitHubCalendar(\".calendar\", \"NoahPeeters\",",
         "categories": null,
         "tags": null,
         "url": "https://www.noahpeeters.de/",
