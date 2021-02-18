a58fe781dbb34b2652ecd9e54023136181afb2d2
NoahPeeters <noah@noahpeeters.de>
2021-02-18 23:31:08 +0100

    Digital document download

---
+++ assets/js/main.ts
@@ -0,0 +1,21 @@
+
+class DigitalDocumentService {
+    private static baseURL = 'https://link.noahpeeters.de'
+    private static targetURLSearchParamKey = 'targetURL'
+
+    documentURL(documentCode: string): URL {
+        return new URL('/' + documentCode, DigitalDocumentService.baseURL);
+    }
+
+    documentURLFromInput(inputID: string): URL {
+        let code: string = (document.getElementById('document-code') as HTMLFormElement).value
+        return this.documentURL(code);
+    }
+
+    openDocumentFromInput(inputID: string) {
+        window.location.href = this.documentURLFromInput(inputID).href;
+    }
+}
+
+
+(window as any).DigitalDocumentService = new DigitalDocumentService();
